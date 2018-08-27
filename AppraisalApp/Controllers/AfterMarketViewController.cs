391104b34ee54a4fca7d2d0bb08f4b51d45dc00f
chidambar.dr <chidambar.dr@slkgroup.com>
2018-08-27 14:42:07 +0530

    afterMarket Factory option

--- AppraisalApp/Controllers/AfterMarketViewController.cs
+++ AppraisalApp/Controllers/AfterMarketViewController.cs
@@ -9,5 +9,20 @@ namespace AppraisalApp
         public AfterMarketViewController (IntPtr handle) : base (handle)
         {
         }
+        public static class globalInde
+        {
+            public static string selectedSegmentIndex = null;
+            public static string oldselectedSegmentIndex = null;
+        }
+        partial void SegmentValue_Changed(UISegmentedControl sender)
+        {
+            string segmentID = ReconditionSegment.SelectedSegment.ToString();
+            if ((globalInde.selectedSegmentIndex != null))
+            {
+                globalInde.oldselectedSegmentIndex = globalInde.selectedSegmentIndex.ToString();
+            }
+            globalInde.selectedSegmentIndex = segmentID.ToString();
+           
+        }
     }
 }
