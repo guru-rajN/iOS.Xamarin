71f7abf65aed8823cda2715b3a188a363c55d7a5
Sathya <sathyabrata.panda@slkgroup.com>
2018-08-31 15:33:07 +0530

    Dealer id changes

--- AppraisalApp/Services/ServiceImpl.cs
+++ AppraisalApp/Services/ServiceImpl.cs
@@ -107,7 +107,7 @@ namespace ExtAppraisalApp.Services
                 {
                     result = null;
 
-                    Utilities.Utility.ShowAlert("Appraisal App", "Please Enter Valid Zip/Dealer Code!!", "OK");
+                   // Utilities.Utility.ShowAlert("Appraisal App", "Please Enter Valid Zip/Dealer Code!!", "OK");
                 }
 
             }
