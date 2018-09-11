2c9cbfccc4599402be1699ef05a0e58b75d1048e
chidambar.dr <chidambar.dr@slkgroup.com>
2018-09-11 13:00:10 +0530

    apprasial log changes

--- AppraisalApp/Controllers/AppraisalLogCell.cs
+++ AppraisalApp/Controllers/AppraisalLogCell.cs
@@ -1,6 +1,7 @@
 using AppraisalApp.Models;
 using Foundation;
 using System;
+using System.Globalization;
 using UIKit;
 
 namespace AppraisalApp
@@ -18,7 +19,8 @@ namespace AppraisalApp
             Mileage.Text = Convert.ToString(amfactoryOption.Mileage);
             CreatedBy.Text = amfactoryOption.Appraisedby;
             Trim.Text = amfactoryOption.SeriesTrim;
-            appraisalDate.Text = Convert.ToString(amfactoryOption.CreatedDate);
+            string[] datetime = Convert.ToString(amfactoryOption.CreatedDate).Split(' ');
+            appraisalDate.Text = datetime[0]; 
             string[] tokens = amfactoryOption.SACAppraisalValue.Split(',');
             if(amfactoryOption.SACAppraisalValue!=""){
                 sacComment.Text = Convert.ToString(tokens[0]) + " " + "$" + " " + tokens[1];
